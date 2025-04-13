2940b1315d52b956f1c67190aab85ed00491072f
Chibuike Osita <mozart.osita@gmail.com>
2025-04-13 21:27:24 +0100

    chore(updates): Cleanups and syncs

--- array/slices_test.go
+++ array/slices_test.go
@@ -1,10 +1,19 @@
 package array
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
+	checkSum := func(t testing.TB, got, want []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 
@@ -26,5 +35,26 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, number)
 		}
+	})
+
+	t.Run("Sum all functionalities", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		checkSum(t, got, want)
+	})
+
+	t.Run("Sum All Tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSum(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSum(t, got, want)
 	})
 }
--- array/sum.go
+++ array/sum.go
@@ -14,3 +14,28 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, 0, lengthOfNumbers)
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+
+	return sums
+}
