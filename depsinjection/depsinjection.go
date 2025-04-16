5d8a3412ddd4aaaec97859fbd76e2cdf5d9f7d8f
Chibuike Osita <mozart.osita@gmail.com>
2025-04-16 13:24:03 +0100

    chore(updates): docs

--- depsinjection/depsinjection.go
+++ depsinjection/depsinjection.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 )
 
+// Interfaces are the key to golang's Dependency injections
+// both io.Writer and http.ResponseWriter both implement a Write
+// function which takes in []byte as arguements
 func Greet[T string](b io.Writer, name T) {
 	fmt.Fprintf(b, "Hello, %s", name)
 }
